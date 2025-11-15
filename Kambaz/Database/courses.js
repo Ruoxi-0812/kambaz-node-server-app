469b9dc8dc9a00eebcc76822288b3fc211f2134c
Ruoxi Wang <ruoxiwang42@gmail.com>
2025-11-15 06:43:50 -0500

    a5

--- Kambaz/Database/courses.js
+++ Kambaz/Database/courses.js
@@ -1,44 +1,45 @@
 export default [
-  {
-    _id: "4550",
-    name: "CS4550 Web Development",
-    description: "Full Stack software developer",
-    image: "/images/reactjs.jpg",
-  },
-  {
-    _id: "4400",
-    name: "CS4400 Programming Languages",
-    description: "202610_1 Fall 2025 Semester Full Term",
-    image: "/images/4400.jpg",
-  },
-  {
-    _id: "4700",
-    name: "CS4700 Network Fundamentals",
-    description: "202610_1 Fall 2025 Semester Full Term",
-    image: "/images/4700.jpg",
-  },
-  {
-    _id: "3650",
-    name: "CS3650 Computer Systems",
-    description: "202510_1 Fall 2024 Semester Full Term",
-    image: "/images/3650.jpg",
-  },
-  {
-    _id: "2800",
-    name: "CS2800 Logic and Computation",
-    description: "202530_1 Spring 2024 Semester Full Term",
-    image: "/images/2800.jpg",
-  },
-  {
-    _id: "1800",
-    name: "CS1800 Discrete Structures",
-    description: "202410_1 Fall 2023 Semester Full Term",
-    image: "/images/1800.jpg",
-  },
-  {
-    _id: "1210",
-    name: "CS1210 Professional Development",
-    description: "202510_1 Fall 2024 Semester Full Term",
-    image: "/images/1210.jpg",
-  },
-];
+    {
+      "_id": "4550",
+      "name": "CS4550 Web Development",
+      "description": "Full Stack software developer",
+      "image": "/images/reactjs.jpg"
+    },
+    {
+      "_id": "4400",
+      "name": "CS4400 Programming Languages",
+      "description": "202610_1 Fall 2025 Semester Full Term",
+      "image": "/images/4400.jpg"
+    },
+    {
+      "_id": "4700",
+      "name": "CS4700 Network Fundamentals",
+      "description": "202610_1 Fall 2025 Semester Full Term",
+      "image": "/images/4700.jpg"
+    },
+    {
+      "_id": "3650",
+      "name": "CS3650 Computer Systems",
+      "description": "202510_1 Fall 2024 Semester Full Term",
+      "image": "/images/3650.jpg"
+    },
+    {
+      "_id": "2800",
+      "name": "CS2800 Logic and Computation",
+      "description": "202530_1 Spring 2024 Semester Full Term",
+      "image": "/images/2800.jpg"
+    },
+    {
+      "_id": "1800",
+      "name": "CS1800 Discrete Structures",
+      "description": "202410_1 Fall 2023 Semester Full Term",
+      "image": "/images/1800.jpg"
+    },
+    {
+      "_id": "1210",
+      "name": "CS1210 Professional Development",
+      "description": "202510_1 Fall 2024 Semester Full Term",
+      "image": "/images/1210.jpg"
+    }
+  ]
+  
--- Kambaz/Database/enrollments.js
+++ Kambaz/Database/enrollments.js
@@ -1,32 +1,34 @@
 export default [
-  { _id: "1", user: "123", course: "4550" },
-  { _id: "2", user: "234", course: "4550" },
-  { _id: "3", user: "345", course: "4550" },
-  { _id: "4", user: "456", course: "4550" },
-  { _id: "5", user: "567", course: "4550" },
-  { _id: "5", user: "899", course: "4550" },
+    { "_id": "1",  "user": "123", "course": "4550" },
+    { "_id": "2",  "user": "234", "course": "4550" },
+    { "_id": "3",  "user": "345", "course": "4550" },
+    { "_id": "4",  "user": "456", "course": "4550" },
+    { "_id": "5",  "user": "567", "course": "4550" },
+    { "_id": "5",  "user": "899", "course": "4550" },
 
-  { _id: "6", user: "234", course: "4400" },
-  { _id: "7", user: "789", course: "4400" },
-  { _id: "8", user: "890", course: "4400" },
-  { _id: "9", user: "123", course: "4400" },
 
-  { _id: "10", user: "789", course: "4700" },
-  { _id: "11", user: "890", course: "4700" },
-  { _id: "5", user: "899", course: "4700" },
-
-  { _id: "12", user: "345", course: "3650" },
-  { _id: "13", user: "456", course: "3650" },
-
-  { _id: "14", user: "567", course: "2800" },
-  { _id: "15", user: "123", course: "2800" },
-  { _id: "5", user: "899", course: "2800" },
-
-  { _id: "16", user: "234", course: "1800" },
-  { _id: "17", user: "123", course: "1800" },
-  { _id: "5", user: "899", course: "1800" },
-
-  { _id: "18", user: "789", course: "1210" },
-  { _id: "19", user: "567", course: "1210" },
-  { _id: "5", user: "899", course: "1210" },
-];
+    { "_id": "6",  "user": "234", "course": "4400" },
+    { "_id": "7",  "user": "789", "course": "4400" },
+    { "_id": "8",  "user": "890", "course": "4400" },
+    { "_id": "9",  "user": "123", "course": "4400" },
+  
+    { "_id": "10", "user": "789", "course": "4700" },
+    { "_id": "11", "user": "890", "course": "4700" },
+    { "_id": "5",  "user": "899", "course": "4700" },
+  
+    { "_id": "12", "user": "345", "course": "3650" },
+    { "_id": "13", "user": "456", "course": "3650" },
+  
+    { "_id": "14", "user": "567", "course": "2800" },
+    { "_id": "15", "user": "123", "course": "2800" },
+    { "_id": "5",  "user": "899", "course": "2800" },
+  
+    { "_id": "16", "user": "234", "course": "1800" },
+    { "_id": "17", "user": "123", "course": "1800" },
+    { "_id": "5",  "user": "899", "course": "1800" },
+  
+    { "_id": "18", "user": "789", "course": "1210" },
+    { "_id": "19", "user": "567", "course": "1210" },
+    { "_id": "5",  "user": "899", "course": "1210" }
+  ]
+  
