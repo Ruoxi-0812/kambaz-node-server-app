ef1e820389cb292b6911e794caff3559c0095228
Ruoxi Wang <ruoxiwang42@gmail.com>
2025-11-15 06:57:06 -0500

    a5

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import express from 'express'
-import Hello from "./Hello.js"
+import express from "express";
+import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
 import cors from "cors";
 import db from "./Kambaz/Database/index.js";
@@ -11,43 +11,41 @@ import ModulesRoutes from "./Kambaz/Modules/routes.js";
 import AssignmentsRoutes from "./Kambaz/Assignments/routes.js";
 import EnrollmentsRoutes from "./Kambaz/Enrollments/routes.js";
 
-const app = express()
+const app = express();
+
+app.set("trust proxy", 1);
+
+const isDev = process.env.SERVER_ENV === "development";
+
 app.use(
   cors({
     origin: process.env.CLIENT_URL || "http://localhost:3000",
     credentials: true,
-    allowedHeaders: ["Content-Type"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
-const sessionOptions = {
-  secret: process.env.SESSION_SECRET || "kambaz",
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    secure: false,         
-    httpOnly: false,    
-    sameSite: "lax",    
-  },
-};
 
-if (process.env.SERVER_ENV !== "development") {
-  sessionOptions.proxy = true;
-  sessionOptions.cookie = {
-    sameSite: "none",
-    secure: true,
-    domain: process.env.SERVER_URL,
-  };
-}
-app.use(session(sessionOptions));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || "kambaz",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      sameSite: isDev ? "lax" : "none",
+      secure: isDev ? false : true,
+      httpOnly: true,
+    },
+  })
+);
+
 app.use(express.json());
+
 UserRoutes(app, db);
 CourseRoutes(app, db);
 ModulesRoutes(app, db);
 AssignmentsRoutes(app, db);
 EnrollmentsRoutes(app, db);
 
-Hello(app)
+Hello(app);
 Lab5(app);
 
-app.listen(process.env.PORT || 4000)
+app.listen(process.env.PORT || 4000);
