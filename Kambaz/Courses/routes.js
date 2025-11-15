4f726cb13d5c2a38c85851476348cf6c7d1ac3d1
Ruoxi Wang <ruoxiwang42@gmail.com>
2025-11-15 04:42:10 -0500

    a5

--- Kambaz/Courses/routes.js
+++ Kambaz/Courses/routes.js
@@ -5,10 +5,23 @@ export default function CourseRoutes(app, db) {
   const dao = CoursesDao(db);
   const enrollmentsDao = EnrollmentsDao(db);
 
+  const createCourse = (req, res) => {
+    const currentUser = req.session["currentUser"];
+
+    if (!currentUser) {                    
+      return res.sendStatus(401);
+    }
+
+    const newCourse = dao.createCourse(req.body);
+    enrollmentsDao.enrollUserInCourse(currentUser._id, newCourse._id);
+    res.json(newCourse);
+  };
+  app.post("/api/users/current/courses", createCourse);
+
   const findAllCourses = (req, res) => {
     const courses = dao.findAllCourses();
-    res.json(courses);
-  };
+    res.send(courses);
+  }
 
   const findCoursesForEnrolledUser = (req, res) => {
     let { userId } = req.params;
@@ -23,34 +36,19 @@ export default function CourseRoutes(app, db) {
     const courses = dao.findCoursesForEnrolledUser(userId);
     res.json(courses);
   };
-
-  const createCourse = (req, res) => {
-    const currentUser = req.session["currentUser"];
-    if (!currentUser) {
-      res.sendStatus(401);
-      return;
-    }
-    const newCourse = dao.createCourse(req.body);
-    enrollmentsDao.enrollUserInCourse(currentUser._id, newCourse._id);
-    res.json(newCourse);
-  };
-
-  const deleteCourse = (req, res) => {
+    const deleteCourse = (req, res) => {
     const { courseId } = req.params;
     const status = dao.deleteCourse(courseId);
     res.send(status);
-  };
-
-  const updateCourse = (req, res) => {
+  }
+    const updateCourse = (req, res) => {
     const { courseId } = req.params;
     const courseUpdates = req.body;
     const status = dao.updateCourse(courseId, courseUpdates);
     res.send(status);
-  };
-
-  app.get("/api/courses", findAllCourses);
-  app.get("/api/users/:userId/courses", findCoursesForEnrolledUser);
-  app.post("/api/users/current/courses", createCourse);
-  app.delete("/api/courses/:courseId", deleteCourse);
+  }
   app.put("/api/courses/:courseId", updateCourse);
+  app.delete("/api/courses/:courseId", deleteCourse);
+  app.get("/api/users/:userId/courses", findCoursesForEnrolledUser);
+  app.get("/api/courses", findAllCourses);
 }
