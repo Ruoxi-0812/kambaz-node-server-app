985c4254503207ab88346e97713b94c87b514796
Ruoxi Wang <ruoxiwang42@gmail.com>
2025-11-15 03:30:50 -0500

    a5

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
-import express from "express";
-import Hello from "./Hello.js";
+import express from 'express'
+import Hello from "./Hello.js"
 import Lab5 from "./Lab5/index.js";
 import cors from "cors";
-import PathParameters from "./Lab5/PathParameters.js";
-import QueryParameters from "./Lab5/QueryParameters.js";
-import WorkingWithArrays from "./Lab5/WorkingWithArrays.js";
-import WorkingWithObjects from "./Lab5/WorkingWithObjects.js";
 import db from "./Kambaz/Database/index.js";
 import UserRoutes from "./Kambaz/Users/routes.js";
+import "dotenv/config";
+import session from "express-session";
 import CourseRoutes from "./Kambaz/Courses/routes.js";
 import ModulesRoutes from "./Kambaz/Modules/routes.js";
 import AssignmentsRoutes from "./Kambaz/Assignments/routes.js";
 import EnrollmentsRoutes from "./Kambaz/Enrollments/routes.js";
-import PeopleRoutes from "./Kambaz/People/routes.js";
-import "dotenv/config";
-import session from "express-session";
-const app = express();
+
+const app = express()
 app.use(
   cors({
-    credentials: true,
     origin: process.env.CLIENT_URL || "http://localhost:3000",
+    credentials: true,
+    allowedHeaders: ["Content-Type"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
 const sessionOptions = {
   secret: process.env.SESSION_SECRET || "kambaz",
   resave: false,
   saveUninitialized: false,
+  cookie: {
+    secure: false,         
+    httpOnly: false,    
+    sameSite: "lax",    
+  },
 };
 
-if (process.env.SERVER_ENV === "production") {
+if (process.env.SERVER_ENV !== "development") {
   sessionOptions.proxy = true;
   sessionOptions.cookie = {
     sameSite: "none",
@@ -36,19 +39,15 @@ if (process.env.SERVER_ENV === "production") {
     domain: process.env.SERVER_URL,
   };
 }
-
 app.use(session(sessionOptions));
 app.use(express.json());
-Lab5(app);
-PathParameters(app);
-QueryParameters(app);
-WorkingWithObjects(app);
-WorkingWithArrays(app);
-Hello(app);
 UserRoutes(app, db);
 CourseRoutes(app, db);
 ModulesRoutes(app, db);
 AssignmentsRoutes(app, db);
 EnrollmentsRoutes(app, db);
-PeopleRoutes(app, db);
-app.listen(process.env.PORT || 4000);
+
+Hello(app)
+Lab5(app);
+
+app.listen(process.env.PORT || 4000)
